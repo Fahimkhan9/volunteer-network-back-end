64654c0c0284d0b32b383d8d3a06964d7ebf6a39
Fahimkhan9 <61534271+Fahimkhan9@users.noreply.github.com>
2020-10-05 15:58:21 +0600

    all-api-created

--- index.js
+++ index.js
@@ -1,83 +1,78 @@
-const express = require('express')
-const cors = require("cors")
-const bodyparser = require("body-parser")
+const express = require("express");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
 
-const MongoClient = require('mongodb').MongoClient;
-require("dotenv").config()
+require("dotenv").config();
 
-
-const app = express()
-
+const app = express();
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vigvf.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-const port = 5000
+const port = 5000;
 
-app.use(cors())
-app.use(bodyparser.json())
+app.use(cors());
+app.use(bodyparser.json());
 
+const urlencoded = bodyparser.urlencoded({ extended: false });
 
-const urlencoded = bodyparser.urlencoded({extended: false})
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 //db connection
 
-const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
-client.connect(err => {
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
   const collection = client.db("volunteerNetwork").collection("registrations");
-  const alldatacollection = client.db("volunteerNetwork").collection("allActivityData");
-//   app.post("/postalldata",urlencoded,(req,res) => {
-//     console.log(req.body);
-//     const alldatainfo = req.body
-// alldatacollection.insertMany(alldatainfo)
-// .then(result => console.log(result.insertedCount))
-// .catch(err => console.log(err))
+  const alldatacollection = client
+    .db("volunteerNetwork")
+    .collection("allActivityData");
 
-  
-//   })
-  
-  app.get("/getalldata",(req,res )=> {
-    alldatacollection.find({})
-    .toArray((err,documents) => {
-      res.send(documents)
-  
-    })
-  })
-    app.post("/addregistrations",urlencoded,(req,res) => {
-      console.log(req.body);
-      const registrationsInfo = req.body
-collection.insertOne(registrationsInfo)
-.then(result => console.log(result.insertedCount))
-.catch(err => console.log(err))
-  
-      })
-  
-  app.get("/getevents",(req,res) => {
-    console.log(req.query.email);
-    const useremail = req.query.email
-   collection.find({email: req.query.email})
-   .toArray((err,documents) => {
-     res.send(documents)
-     console.log(documents);
-   })
-  })
+  app.get("/activitydata", (req, res) => {
+    alldatacollection.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
 
-console.log("db connected");
+  app.post("/addregistrations", urlencoded, (req, res) => {
+    const registrationsInfo = req.body;
+    collection
+      .insertOne(registrationsInfo)
+      .then((result) => console.log(result.insertedCount))
+      .catch((err) => console.log(err));
+  });
 
+  app.get("/getfilteredevents/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    alldatacollection.find({ id: id }).toArray((err, documents) => {
+      res.send(documents[0]);
+    });
+  });
+
+  app.get("/getfilteredevents", (req, res) => {
+    const email = req.query.email;
+    const covertedemail = email.toString();
+    collection.find({ useremail: covertedemail }).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+  app.delete("/deleteevents/:id", (req, res) => {
+    const id = req.params.id;
+    const convertedid = id.toString();
+    console.log(convertedid);
+
+    collection.deleteOne({ registeractivityid: convertedid }).then((result) => {
+      console.log(result.deletedCount);
+    });
+  });
+
+  console.log("db connected");
 });
 
-
-
-
-
-
-
-
-
-
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
